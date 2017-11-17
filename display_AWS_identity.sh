6c070f60e7faf9ca8c572bc99b4beb04d863445d
Stefan Wuensch <stefan_wuensch@harvard.edu>
2017-11-17 16:08:16 -0500

    Fix two minor things suggested by shellcheck.net.

--- display_AWS_identity.sh
+++ display_AWS_identity.sh
@@ -20,6 +20,9 @@
 # un-comment the line near the end that contains "exit".
 # You can also process the return state of this function:
 # 0 -> OK, 1 -> Error
+#
+# This script checked with https://www.shellcheck.net/
+
 
 function display_AWS_identity() {
 
@@ -43,7 +46,7 @@ function display_AWS_identity() {
 		# Note 1: This is doing a process-substitution so that we can use "read" to get three variables set at once
 		# in the easiest possible way. http://www.tldp.org/LDP/abs/html/process-sub.html
 		# Note 2: The 'awk' is using a regex so that we can use _both_ ':' and '/' as field separators. Sweet!!
-		read accountID roleName HKey_email < <( aws sts get-caller-identity --query=Arn --output=text 2>/dev/null | awk -F'[:/]' '{print $5,$7,$8}' )
+		read -r accountID roleName HKey_email < <( aws sts get-caller-identity --query=Arn --output=text 2>/dev/null | awk -F'[:/]' '{print $5,$7,$8}' )
 	fi
 
 	# Now - finally - if we had any problems on the first attempts, and we _did_ get something
@@ -56,7 +59,7 @@ function display_AWS_identity() {
 
 	# If after all of that the output contains "error" we can be 100% certain that the credentials
 	# are busted (meaning the IAM user account is dead) for the profile specified or current session.
-	if echo ${username} ${accountName} | grep -q error ; then
+	if echo "${username} ${accountName}" | grep -q error ; then
 		echo "ERROR: Problem validating active AWS session / credentials. You may need to check or refresh your access keys or STS."
 		# exit 1	# Un-comment this line to exit here if there's a problem. Otherwise...
 		return 1	# This allows you to act upon the result of this function if you want.
